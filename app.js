f6b5f9cd49eaaf374dd245413a740f62fd16dbea
asifahmed786 <asifahmed1us1@gmail.com>
2020-07-09 23:07:50 +0500

    final stop watch

--- app.js
+++ app.js
@@ -5,34 +5,66 @@ var interval;
 var elemmsec = document.getElementById("msec");
 var elemsec = document.getElementById("sec");
 var elemmin = document.getElementById("min");
+var lap1 = document.getElementById("lap1")
+var timerStatus = true;
+var count = 0;
 
-function timer1(){
+function timer1(){    
     msec++;
+    if(msec==100){
+        elemmsec.innerHTML = "00";
+    }
+    else if(msec>9){
+        elemmsec.innerHTML = msec;
+    }
+    else{
+        elemmsec.innerHTML = "0"+msec;
+    }    
     
-    elemmsec.innerHTML = msec;
     if(msec>=100){
         msec = 0;
         sec++
-        elemsec.innerHTML = sec;
+        if(sec>9){
+            elemsec.innerHTML = sec;
+            
+        }
+        else{
+            elemsec.innerHTML = "0"+sec;
+        }
 
     }
     else if(sec>=60){
         sec = 0;
         min++;
-        elemmin.innerHTML = min;
+        if(min>9){
+            elemmin.innerHTML = min;
+        }
+        else{
+            elemmin.innerHTML = "0"+min;
+        }
+        
 
     }
 }
 function start(){
     // alert("hello");
-    interval = setInterval(timer1,10)
+    if(timerStatus){
+        interval = setInterval(timer1,10)
+        timerStatus = false;
+    }
+    else{
+        alert("stop watch is already running")
+        // timerStatus = false
+    }
+    
     
     
 }
 
 function pause(){
     // alert("this is pause");
-    clearInterval(interval)
+    clearInterval(interval);
+    
 }
 
 function clear1(){
@@ -41,10 +73,22 @@ function clear1(){
     min = 0;
     sec = 0;
     msec  =0;
-    elemmin.innerHTML = "0";
-    elemsec.innerHTML = "0"
-    elemmsec.innerHTML = "0"
+    elemmin.innerHTML = "0"+min;
+    elemsec.innerHTML = "0"+sec
+    elemmsec.innerHTML = "0"+msec
+    lap1.innerHTML ="";
+    timerStatus = true;
+    count=0;
 }
 function lap(){
-    alert("this is lap")
+    if(timerStatus==true){
+
+    }
+    else{
+        count++;
+        // alert("this is lap")        
+        lap1.innerHTML += "<p class='text-center text-light'>Lap "+count+" "+min+":"+sec+":"+msec+"<p>"
+        // document.write("<p class='text-center'>Lap "+min+":"+sec+":"+msec+"<p>")
+    }
+    
 }
